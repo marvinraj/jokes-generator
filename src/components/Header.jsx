624c59c6d61594444acb9aaf0de5c50a2110db03
Marvin <marvinraj77@gmail.com>
2024-10-01 08:50:53 +0800

    done with header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1>jokes generator</h1>
+        <p>if you laugh, you die</p>
+    </div>
+  )
+}
+
+export default Header
